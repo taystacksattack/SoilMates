b918ffc9f46482d4b9d8903d17b8acd339a64b64
Taylor McClerin <mcclerinette@gmail.com>
2023-06-15 15:10:13 -0700

    api fetch is WORKINGgit add .git add .

--- react-app/src/components/SoilsFetch/dataParsers.js
+++ react-app/src/components/SoilsFetch/dataParsers.js
@@ -3,65 +3,80 @@ import { sampleData } from "./sampleData";
 // export const test = () => console.log("testghfuishgfkdhgfhdsghfdjkghfdjkg")
 // export const test = () => console.log(sampleData.properties.layers[5])
 
+//collect soil data
+
+//lat and long string-interpolated. THIS ACTUALLY WORKS!!!
+export const getSoilData = async (longitude, latitude) => {
+    const response = await fetch(`https://rest.isric.org/soilgrids/v2.0/properties/query?lon=${longitude}&lat=${latitude}&property=bdod&property=cec&property=clay&property=nitrogen&property=phh2o&property=sand&property=silt&property=soc&depth=0-5cm&depth=0-30cm&depth=5-15cm&depth=15-30cm&depth=30-60cm&value=Q0.5&value=mean&value=uncertainty`)
+    const data = await response.json()
+    return data
+}
+
+
+
+
+
+
+
 // this gives you the whole data object parsed to the specific elements/properties - is an array (note the keying in of "layers")
 export const dataProperties = sampleData.properties
 
 // free free to comment/uncomment things
-export const sandParse = () => {
-    const sandObjData =sampleData.properties.layers[5].depths
-    // const sandObjValue =sampleData.properties.layers[5].depths[0].values["Q0.5"]
-    // console.log("sand Object data", sandObjData)// [0].values["Q0.5"]
+export const sandParse = (data) => {
+    const objData =data.properties.layers[5].depths
+    // const sandObjValue =data.properties.layers[5].depths[0].values["Q0.5"]
+    // console.log("sand Object data", objData)// [0].values["Q0.5"]
     // console.log(sandObjValue)
-    // const reducedMedians = sandObjData.forEach((median) => console.log(median))//.depths[0].values["Q0.5"], 0)
-    const reducedMedians = sandObjData.reduce((acc, median) => acc + median.values["Q0.5"], 0)
+    // const reducedMedians = objData.forEach((median) => console.log(median))//.depths[0].values["Q0.5"], 0)
+    const reducedMedians = objData.reduce((acc, median) => acc + median.values["Q0.5"], 0)
     // console.log("REDUCED MEDIANS", reducedMedians)
-    return (reducedMedians / sandObjData.length)/10
+    return (reducedMedians / objData.length)/10
 }
 // needs to be divided by 10 to convert from mapped unites to conventional units
 
 
 // The rest of this is plug and chug: keys differ, but same mechanics. Math may differ based on mapped to conventional unit conversion.
 
-export const siltParse = () => {
-    const sandObjData =sampleData.properties.layers[6].depths
-    const reducedMedians = sandObjData.reduce((acc, median) => acc + median.values["Q0.5"], 0)
-    return (reducedMedians / sandObjData.length) / 10
+export const siltParse = (data) => {
+    const objData =data.properties.layers[6].depths
+    const reducedMedians = objData.reduce((acc, median) => acc + median.values["Q0.5"], 0)
+    return (reducedMedians / objData.length) / 10
 }
 
 
-export const clayParse = () => {
-    const sandObjData =sampleData.properties.layers[2].depths
-    const reducedMedians = sandObjData.reduce((acc, median) => acc + median.values["Q0.5"], 0)
-    return (reducedMedians / sandObjData.length) / 10
+export const clayParse = (data) => {
+    const objData =data.properties.layers[2].depths
+    const reducedMedians = objData.reduce((acc, median) => acc + median.values["Q0.5"], 0)
+    return (reducedMedians / objData.length) / 10
 }
 
 
-export const cecParse = () => {
-    const sandObjData =sampleData.properties.layers[2].depths
-    const reducedMedians = sandObjData.reduce((acc, median) => acc + median.values["Q0.5"], 0)
-    return (reducedMedians / sandObjData.length) / 10
+export const cecParse = (data) => {
+    const objData =data.properties.layers[2].depths
+    const reducedMedians = objData.reduce((acc, median) => acc + median.values["Q0.5"], 0)
+    return (reducedMedians / objData.length) / 10
 }
 
-export const bdodParse = () => {
-    const sandObjData =sampleData.properties.layers[0].depths
-    const reducedMedians = sandObjData.reduce((acc, median) => acc + median.values["Q0.5"], 0)
-    return (reducedMedians / sandObjData.length) / 100
+export const bdodParse = (data) => {
+    const objData =data.properties.layers[0].depths
+    const reducedMedians = objData.reduce((acc, median) => acc + median.values["Q0.5"], 0)
+    return (reducedMedians / objData.length) / 100
 }
 
-export const nitrogenParse = () => {
-    const sandObjData =sampleData.properties.layers[3].depths
-    const reducedMedians = sandObjData.reduce((acc, median) => acc + median.values["Q0.5"], 0)
-    return (reducedMedians / sandObjData.length) / 100
+export const nitrogenParse = (data) => {
+    const objData =data.properties.layers[3].depths
+    const reducedMedians = objData.reduce((acc, median) => acc + median.values["Q0.5"], 0)
+    return (reducedMedians / objData.length) / 100
 }
 
-export const socParse = () => {
-    const sandObjData =sampleData.properties.layers[7].depths
-    const reducedMedians = sandObjData.reduce((acc, median) => acc + median.values["Q0.5"], 0)
-    return (reducedMedians / sandObjData.length) / 10
+export const socParse = (data) => {
+    const objData =data.properties.layers[7].depths
+    const reducedMedians = objData.reduce((acc, median) => acc + median.values["Q0.5"], 0)
+    return (reducedMedians / objData.length) / 10
 }
 
-export const pph2oParse = () => {
-    const sandObjData =sampleData.properties.layers[4].depths
-    const reducedMedians = sandObjData.reduce((acc, median) => acc + median.values["Q0.5"], 0)
-    return (reducedMedians / sandObjData.length) / 10
+export const pph2oParse = (data) => {
+    const objData =data.properties.layers[4].depths
+    const reducedMedians = objData.reduce((acc, median) => acc + median.values["Q0.5"], 0)
+    return (reducedMedians / objData.length) / 10
 }
--- react-app/src/components/SoilsFetch/index.js
+++ react-app/src/components/SoilsFetch/index.js
@@ -2,7 +2,8 @@ import { useDispatch } from "react-redux";
 import { useState,useEffect } from 'react';
 // import { createPostThunk } from '../../store/posts'
 import { useHistory } from 'react-router-dom';
-import { dataProperties, sandParse, siltParse, clayParse, cecParse, bdodParse, nitrogenParse, socParse, pph2oParse } from "./dataParsers";
+import { sampleData } from "./sampleData";
+import { getSoilData, dataProperties, sandParse, siltParse, clayParse, cecParse, bdodParse, nitrogenParse, socParse, pph2oParse } from "./dataParsers";
 
 
 const SoilsFetch = () => {
@@ -34,21 +35,21 @@ const SoilsFetch = () => {
         if(Object.values(validationErrors).length) {
             return
         }
-        //lat and long string-interpolated THIS ACTUALLY WORKS - COMMENT BACK IN WHEN READY
-        // const response = await fetch(`https://rest.isric.org/soilgrids/v2.0/properties/query?lon=${longitude}&lat=${latitude}&property=bdod&property=cec&property=clay&property=nitrogen&property=phh2o&property=sand&property=silt&property=soc&depth=0-5cm&depth=0-30cm&depth=5-15cm&depth=15-30cm&depth=30-60cm&value=Q0.5&value=mean&value=uncertainty`)
-        // const data = await response.json()
-        // console.log("here is the jsonified data", data)
 
-        // console.log("SAMPLE DATA S@#%SHOW", sampleData)
+        // const data = sampleData
+        const data = await getSoilData(longitude, latitude)
+
+        console.log("WHOLE DATA S@#%SHOW", data)
         console.log("data data data", dataProperties)
-        setSand(sandParse())
-        setSilt(siltParse())
-        setClay(clayParse())
-        setCec(cecParse())
-        setBdod(bdodParse())
-        setNitrogen(nitrogenParse())
-        setSoc(siltParse())
-        setPph2o(pph2oParse())
+
+        setSand(sandParse(data))
+        setSilt(siltParse(data))
+        setClay(clayParse(data))
+        setCec(cecParse(data))
+        setBdod(bdodParse(data))
+        setNitrogen(nitrogenParse(data))
+        setSoc(siltParse(data))
+        setPph2o(pph2oParse(data))
 
 
 

47aa04172789c9ec98bd47e601cd7373ab545192
Taylor McClerin <mcclerinette@gmail.com>
2023-06-22 14:04:01 -0700

    sign up error handlers working

--- react-app/src/components/SignupFormModal/index.js
+++ react-app/src/components/SignupFormModal/index.js
@@ -22,6 +22,8 @@ function SignupFormModal() {
 	const handleSubmit = async (e) => {
 		e.preventDefault();
 		setHasSubmitted(true)
+		if (Object.values(validationErrors).length) return
+
 		if (password === confirmPassword) {
 			const data = await dispatch(signUp(username, email, password));
 			if (data) {

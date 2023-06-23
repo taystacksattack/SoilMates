1db76f9491d983700b25031182319c2cceb6477f
Taylor McClerin <mcclerinette@gmail.com>
2023-06-23 15:05:23 -0700

    map is rocking, and you can leave a marker which updates the latlng

--- react-app/src/components/SoilsFetch/Map.js
+++ react-app/src/components/SoilsFetch/Map.js
@@ -2,40 +2,75 @@ import React, { useRef, useEffect, useState } from 'react';
 import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
 import './Map.css'
 //private token
-mapboxgl.accessToken = "pk.eyJ1IjoidGF5c3RhY2tzIiwiYSI6ImNsajh5a2R3MjBzaGczaW41YmIwMmpjcDIifQ.b5p7I1Utegs1ltjWasrzyw";
+// mapboxgl.accessToken = "pk.eyJ1IjoidGF5c3RhY2tzIiwiYSI6ImNsajh5a2R3MjBzaGczaW41YmIwMmpjcDIifQ.b5p7I1Utegs1ltjWasrzyw";
 
 //public token
-// mapboxgl.accessToken = "pk.eyJ1IjoidGF5c3RhY2tzIiwiYSI6ImNsajh1eGQ4NjEzODgzZWt4MTMzdmR6enQifQ.TkQPPCuojyM0wguZ3sBLxA";
+mapboxgl.accessToken = "pk.eyJ1IjoidGF5c3RhY2tzIiwiYSI6ImNsajh1eGQ4NjEzODgzZWt4MTMzdmR6enQifQ.TkQPPCuojyM0wguZ3sBLxA";
 
 const Map = () => {
-  const mapContainer = useRef(null);
-  const map = useRef(null);
-  const [lng, setLng] = useState(-70.9);
-  const [lat, setLat] = useState(42.35);
-  const [zoom, setZoom] = useState(9);
+    const mapContainer = useRef(null);
+    const map = useRef(null);
+    const [lng, setLng] = useState(-95.274188);
+    const [lat, setLat] = useState(39.640484);
+    const [zoom, setZoom] = useState(2.74);
+    const [marker, setMarker] = useState(null)
+
+
+    const handleMapClick = (e) => {
+        const { lng, lat } = e.lngLat;
+        setLng(lng);
+        setLat(lat);
+
+        // if (marker) {
+        //     marker.remove();
+        // }
+
+        const newMarker = new mapboxgl.Marker()
+        .setLngLat([lng, lat])
+        .addTo(map.current);
+
+        setMarker(newMarker);
+    };
+
 
   useEffect(() => {
     if (map.current) return; // initialize map only once
 
     map.current = new mapboxgl.Map({
-      container: mapContainer.current,
-      style: 'mapbox://styles/mapbox/streets-v12',
-      center: [lng, lat],
-      zoom: zoom
-    });
-  }, []);
-
-  useEffect(() => {
-    if (!map.current) return; // wait for map to initialize
-    map.current.on('move', () => {
-        setLng(map.current.getCenter().lng.toFixed(6));
-        setLat(map.current.getCenter().lat.toFixed(6));
-        setZoom(map.current.getZoom().toFixed(2));
-        });
+        container: mapContainer.current,
+        style: 'mapbox://styles/mapbox/streets-v12',
+        center: [lng, lat],
+        zoom: zoom,
     });
 
+    map.current.on('click', handleMapClick)
 
-    // console.log(lng)
+    return () => {
+        map.current.off('click', handleMapClick)
+    }
+
+}, []);
+
+
+//works on centering the map over a point, and the point becomes the coordinate
+//   useEffect(() => {
+//     if (!map.current) return; // wait for map to initialize
+//     map.current.on('click', (e) => {
+//         setLng(map.current.getCenter().lng.toFixed(6));
+//         setLat(map.current.getCenter().lat.toFixed(6));
+//         setZoom(map.current.getZoom().toFixed(2));
+
+//         });
+//     });
+
+useEffect(() => {
+    if (!marker) return;
+
+    return () => {
+      marker.remove();
+    };
+  }, [marker]);
+
   return (
     <div>
         <div className="sidebar">

47335229fb5820edd99e49e771bafaac4f0926da
Taylor McClerin <mcclerinette@gmail.com>
2023-06-23 08:46:30 -0700

    styling on the new posts and edit posts pages

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -80,7 +80,7 @@ function App() {
             </Route>
 
             <Route path="/" >
-              <SplashPage />
+              {!userObj?(<SplashPage />):(<Feed/>)}
             </Route>
           </Switch>
         )}
